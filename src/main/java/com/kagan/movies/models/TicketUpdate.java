5e47e7dca7276985afc9ce6d89e6d697ba933e74
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-04-07 15:54:52 +0300

    getter setter

--- src/main/java/com/kagan/movies/models/TicketUpdate.java
+++ src/main/java/com/kagan/movies/models/TicketUpdate.java
@@ -45,11 +45,11 @@ public class TicketUpdate {
         this.comment = comment;
     }
 
-    // public Long getId() {
-    //     return id;
-    // }
+    public Long getId() {
+        return id;
+    }
 
-    // public void setId(Long id) {
-    //     this.id = id;
-    // }    
+    public void setId(Long id) {
+        this.id = id;
+    }    
 }
